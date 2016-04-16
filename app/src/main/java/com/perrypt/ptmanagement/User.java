2e6efb9b6501495ac55ff7d8c20a554f6b9f3ddd
Corey Perry <cperry4888@gmail.com>
2016-04-16 12:19:18 -0400

    Initial commit

---
+++ app/src/main/java/com/perrypt/ptmanagement/User.java
@@ -0,0 +1,22 @@
+package com.perrypt.ptmanagement;
+
+import java.util.UUID;
+
+/**
+ * Created by Grayscript on 4/10/2016.
+ */
+public class User {
+    private String mTitle;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public User() {
+
+    }
+}
